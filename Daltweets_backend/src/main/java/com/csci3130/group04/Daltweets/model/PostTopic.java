ec72d27e3b4c0470a8ff8c15b10259b0b5b58b81
Ibrahim Wassouf <ib981347@dal.ca>
2024-06-12 20:57:26 -0300

    feat: add PostTopic Entity model

---
+++ Daltweets_backend/src/main/java/com/csci3130/group04/Daltweets/model/PostTopic.java
@@ -0,0 +1,49 @@
+package com.csci3130.group04.Daltweets.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class PostTopic{
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int id;
+  
+  @ManyToOne(fetch=FetchType.LAZY)
+  @JoinColumn(name="postID")
+  private Post post;
+
+  @ManyToOne(fetch=FetchType.LAZY)
+  @JoinColumn(name="id")
+  private Topic topic;
+
+  public PostTopic(){
+  }
+
+  public PostTopic(int id, Topic topic, Post post){
+    this.id = id;
+    this.topic = topic;
+    this.post = post;
+  }
+
+  public int getID(){
+    return this.id;
+  }
+
+  public Post getPost(){
+    return this.post;
+  }
+
+  public void setPost(Post post){
+    this.post = post;
+  }
+
+  public Topic getTopic(){
+    return this.topic;
+  } 
+
+  public void setTopic(Topic topic){
+    this.topic = topic;
+  }
+}
+
+
