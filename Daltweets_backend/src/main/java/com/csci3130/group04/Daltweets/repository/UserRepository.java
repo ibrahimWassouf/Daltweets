0a17626fa4b470a98621303856dbde4d8aedf005
Oluwatobiloba Onibudo <ol382056@dal.ca>
2024-06-16 23:43:05 -0300

    feat: adding user repository class

---
+++ Daltweets_backend/src/main/java/com/csci3130/group04/Daltweets/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.csci3130.group04.Daltweets.repository;
+import  com.csci3130.group04.Daltweets.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Integer> {
+    
+}
