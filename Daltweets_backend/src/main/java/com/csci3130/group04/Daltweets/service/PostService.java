b3f4b14ab78e6a4d582cd27b0c9c3f9c992c7d60
Ibrahim Wassouf <ib981347@dal.ca>
2024-06-19 21:23:35 -0300

    feat: declare post service signatures

---
+++ Daltweets_backend/src/main/java/com/csci3130/group04/Daltweets/service/PostService.java
@@ -0,0 +1,13 @@
+package com.csci3130.group04.Daltweets.service;
+
+import java.util.List;
+
+import com.csci3130.group04.Daltweets.model.Post;
+import com.csci3130.group04.Daltweets.model.User;
+
+public interface PostService {
+  Post createPost(Post post); 
+  List<Post> getPostsByUser(User user);
+  List<Post> getPostsByUsers(List<User> users);
+
+}
