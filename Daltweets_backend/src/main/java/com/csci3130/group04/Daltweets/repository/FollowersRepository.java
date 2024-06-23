96bee23a8bc0030860a6f3760ef5f731ab3f1872
Oluwatobiloba Onibudo <ol382056@dal.ca>
2024-06-22 23:35:51 -0300

    feat: created followers repository

---
+++ Daltweets_backend/src/main/java/com/csci3130/group04/Daltweets/repository/FollowersRepository.java
@@ -0,0 +1,26 @@
+package com.csci3130.group04.Daltweets.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.csci3130.group04.Daltweets.model.Followers;
+import com.csci3130.group04.Daltweets.model.User;
+
+@Repository
+public interface FollowersRepository extends JpaRepository<Followers,Integer>{
+    @Query("SELECT f.follower FROM Followers f WHERE f.user.id = :userId")
+    List<User> findFollowerIdsByUserId(@Param("userId") int userId);
+
+    User findUserByFollowerId(User Follower);
+
+    Followers findByUserAndFollower(User user, User follower);
+
+    Followers findFollowersByUserIdAndFollowerId(User user, User follower);
+
+    @Query("SELECT COALESCE(MAX(f.id), 0) FROM Followers f")
+    int findLatestId();
+} 
