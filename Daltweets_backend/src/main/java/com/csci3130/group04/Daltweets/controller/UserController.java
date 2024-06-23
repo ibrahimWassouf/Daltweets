00934afd889251c09803c917547006613111dd39
Oluwatobiloba Onibudo <ol382056@dal.ca>
2024-06-23 14:13:03 -0300

    feat: added signup endpoint to user service

--- Daltweets_backend/src/main/java/com/csci3130/group04/Daltweets/controller/UserController.java
+++ Daltweets_backend/src/main/java/com/csci3130/group04/Daltweets/controller/UserController.java
@@ -5,12 +5,16 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.csci3130.group04.Daltweets.model.User;
+import com.csci3130.group04.Daltweets.model.Login;
+import com.csci3130.group04.Daltweets.service.LoginService;
 import com.csci3130.group04.Daltweets.service.Implementation.UserServiceImplementation;
+import com.csci3130.group04.Daltweets.utils.SignUpRequestDTO;
 
 @RestController
 @RequestMapping("/api/user")
@@ -19,6 +23,25 @@ public class UserController {
     @Autowired
     UserServiceImplementation userService;
 
+    @Autowired
+    LoginService loginService;
+
+    @PostMapping("/signup")
+    User createUser(@RequestBody SignUpRequestDTO createUserRequestDto)
+    {
+        User user = createUserRequestDto.getUser();
+        User createdUser = userService.createUser(user);
+
+        Login login = createUserRequestDto.getLogin();
+
+        login.setUsername(createdUser.getUsername());
+        login.setUser(createdUser);
+
+        loginService.createLogin(login);
+        
+       return createdUser;
+    } 
+    
     @GetMapping("/recommended-users")
     List<User> getRecommendedUsers(@RequestBody Map<String, String> requestBody){
       return userService.getRecommendedUsers(requestBody.get("username"));
