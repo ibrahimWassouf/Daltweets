42338517cd020719264ae132ab25dd324fadeab6
Oluwatobiloba Onibudo <ol382056@dal.ca>
2024-06-26 01:39:55 -0300

    refactor: changing endpoint params to use request body

--- Daltweets_backend/src/main/java/com/csci3130/group04/Daltweets/controller/FollowerController.java
+++ Daltweets_backend/src/main/java/com/csci3130/group04/Daltweets/controller/FollowerController.java
@@ -1,6 +1,7 @@
 package com.csci3130.group04.Daltweets.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,10 +39,10 @@ public class FollowerController {
     }
     
     @PostMapping("/accept")
-    ResponseEntity<Boolean> acceptFollowRequest(@RequestParam String username,@RequestParam String followerName)
+    ResponseEntity<Boolean> acceptFollowRequest(@RequestBody Map<String,String> request)
     {
-        User user = userService.getUserByName(username);
-        User follower = userService.getUserByName(followerName); 
+        User user = userService.getUserByName(request.get("username"));
+        User follower = userService.getUserByName(request.get("followerName")); 
         return new ResponseEntity<Boolean>(followersService.acceptFollowRequest(user, follower), HttpStatus.OK);
     }
 
