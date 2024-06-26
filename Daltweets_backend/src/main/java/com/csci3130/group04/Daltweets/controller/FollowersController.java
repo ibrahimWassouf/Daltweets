48670e9e73c7ac710474ee97f4512f6c4e46e99e
Ibrahim Wassouf <ib981347@dal.ca>
2024-06-25 23:14:37 -0300

    feat: add follower controller and add/delete endpoints

---
+++ Daltweets_backend/src/main/java/com/csci3130/group04/Daltweets/controller/FollowersController.java
@@ -0,0 +1,53 @@
+package com.csci3130.group04.Daltweets.controller;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.csci3130.group04.Daltweets.model.Followers;
+import com.csci3130.group04.Daltweets.model.User;
+import com.csci3130.group04.Daltweets.service.Implementation.FollowersServiceImpl;
+import com.csci3130.group04.Daltweets.service.Implementation.UserServiceImplementation;
+
+
+@CrossOrigin(origins = "*", allowedHeaders = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/followers")
+public class FollowersController {
+
+    @Autowired
+    FollowersServiceImpl followersService;
+
+    @Autowired
+    UserServiceImplementation userService;
+
+    @PostMapping("/add")
+    ResponseEntity<String> addFollower(@RequestBody Map<String, String> requestBody){
+      User follower = userService.getUserByName(requestBody.get("follower"));
+      User user = userService.getUserByName(requestBody.get("user"));
+
+      Followers newFollowing = followersService.addFollower(user, follower);
+      if (newFollowing == null){ return new ResponseEntity<>("Error sending follower request.", HttpStatus.BAD_REQUEST);}
+
+      return new ResponseEntity<>("Follower request sent", HttpStatus.OK);
+    }
+
+    @PostMapping("/delete")
+    ResponseEntity<String> deleteFollower(@RequestBody Map<String, String> requestBody){
+      User follower = userService.getUserByName(requestBody.get("follower"));
+      User user = userService.getUserByName(requestBody.get("user"));
+
+      Boolean removeFollowing = followersService.removeFollower(user, follower);
+      if (removeFollowing == null){ return new ResponseEntity<>("Error deleting follower request.", HttpStatus.BAD_REQUEST);}
+
+      return new ResponseEntity<>("Follower request deleted", HttpStatus.OK);
+    }
+}
