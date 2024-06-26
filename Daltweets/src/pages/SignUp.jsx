5f095d58779011be2a942deab67c3937b8f34c3c
Josh <53712177+JoshuaLomond@users.noreply.github.com>
2024-06-26 14:29:18 -0300

    feat: signup page has username, password, email fields. makes http request to user/signup

--- Daltweets/src/pages/SignUp.jsx
+++ Daltweets/src/pages/SignUp.jsx
@@ -1,11 +1,123 @@
-// src/pages/Signup.jsx
-
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 function SignUp() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSignUp = () => {
+    if (username === "" || password === "" || email === "") {
+      setError("All fields are required");
+      return;
+    }
+
+    const signUpData = {
+      user: {
+        username: username,
+        email: email,
+      },
+      login: {
+        password: password,
+      },
+    };
+
+    axios
+      .post(
+        `${import.meta.env.VITE_BACKEND_BASE_URL}/api/user/signup`,
+        signUpData
+      )
+      .then((response) => {
+        console.log(response);
+        setError("");
+      })
+      .catch((error) => {
+        console.log(error);
+        setError(error.response.data);
+      });
+  };
+
   return (
-    <div>
-      <p>Signup-page works!</p>
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+        Sign Up
+      </h2>
+      <br />
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form className="space-y-6">
+          <div>
+            <label
+              htmlFor="username"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Username
+            </label>
+            <div>
+              <input
+                id="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                className="block w-full rounded-md border-0 py-1.5 ps-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Email
+            </label>
+            <div>
+              <input
+                id="email"
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="block w-full rounded-md border-0 py-1.5 ps-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div>
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Password
+            </label>
+            <div className="mt-2">
+              <input
+                id="password"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="block w-full rounded-md border-0 py-1.5 ps-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+        </form>
+
+        {error && <div className="text-red-500 mt-4">{error}</div>}
+
+        <div>
+          <button
+            type="submit"
+            onClick={handleSignUp}
+            className="mt-10 flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Sign Up
+          </button>
+        </div>
+      </div>
+      <p className="mt-10 text-center text-sm text-gray-500">
+        <a
+          href="/login"
+          className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+        >
+          Login
+        </a>
+      </p>
     </div>
   );
 }
