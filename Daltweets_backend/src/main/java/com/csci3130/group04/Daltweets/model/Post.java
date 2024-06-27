b67e0bbbd92b1f3b466a641071c9369a97d1076f
Hoang Le <syhoangloe@gmail.com>
2024-06-26 21:23:59 -0300

    fix: change the pattern for DATE in post model

--- Daltweets_backend/src/main/java/com/csci3130/group04/Daltweets/model/Post.java
+++ Daltweets_backend/src/main/java/com/csci3130/group04/Daltweets/model/Post.java
@@ -18,7 +18,7 @@ public class Post {
 
     private String text;
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime dateCreated;
 
     private boolean isDeleted;
