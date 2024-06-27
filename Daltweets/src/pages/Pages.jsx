5bf7b4f19975af5854e2dabf53e604087508a746
Oluwatobiloba Onibudo <ol382056@dal.ca>
2024-06-27 19:30:48 -0300

    Merge branch 'dev' of https://git.cs.dal.ca/courses/2024-summer/csci3130/Group04 into 54-create-a-page-for-add-delete-friends

--- Daltweets/src/pages/Pages.jsx
+++ Daltweets/src/pages/Pages.jsx
@@ -1,25 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import HomePage from './HomePage';
 import NavBar from '../components/NavBar';
-
+import CreatePost from './CreatePost';
 
 import {
     BrowserRouter as Router,
     Route,
     Routes,
-    Navigate,
   } from "react-router-dom";
 import Friends from './Friends';
-
+import Error from './Error';
 const Pages = () => {
+  const user = localStorage.getItem("user");
   return (
+    <>
+    {
+      user ? (
     <div className = "flex ">
             <NavBar/>
             <Routes>
                 <Route path="/home" element={<HomePage/>} />
                 <Route path="/friends" element={<Friends />} />
+                <Route path="/create" element = {<CreatePost/>} />
             </Routes>
-    </div>
+    </div>) : 
+    (
+        <Error /> 
+    )
+    }
+    </>
   )
 }
 
