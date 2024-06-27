0c62cbe05217028e4494f1568b6202e3059b3c2f
Hoang Le <syhoangloe@gmail.com>
2024-06-27 13:47:31 -0300

    feat: add a create post request in frontend

--- Daltweets/src/pages/CreatePost.jsx
+++ Daltweets/src/pages/CreatePost.jsx
@@ -2,16 +2,32 @@ import React, {useState} from 'react'
 import axios from "axios";
 
 const CreatePost = () => {
-  const [tweetPost,setTweetPost] = useState('');
+  const [id,setId] = useState(1);
+  const [text,setText] = useState('');
+  const [user,setUser] = useState(null);
+  const [dateCreated,setDateCreated] = useState(null);
+  const [isDeleted,setIsDeleted] = useState(false);
+  const [isEdited,setIsEdited] = useState(false);
+
   const sendPost = (e) => {
     e.preventDefault();
+    setUser(JSON.parse(localStorage.getItem('user')));
+    const formData = {
+      id,
+      text,
+      user,
+      dateCreated,
+      isDeleted,
+      isEdited,
+    }
     axios.post(`${import.meta.env.VITE_BACKEND_BASE_URL}/api/post/create`,
-      {
-        tweetPost,
-      }) .then((response) => {
+      formData
+    ) .then((response) => {
+        alert('Post sent success');
         console.log(response);
       })
       .catch((error) => {
+        alert('Post not sent');
         console.log(error);
       });
   }
@@ -19,7 +35,7 @@ const CreatePost = () => {
     <div className="w-screen h-screen">
         CreatePost
         <form className="w-2/3">
-          <textarea className="w-full h-40 border border-yellow-500" type = "text" value = {tweetPost} onChange={(e) => setTweetPost(e.target.value)} placeholder="What's on your mind"/>
+          <textarea className="w-full h-40 border border-yellow-500" type = "text" value = {text} onChange={(e) => setText(e.target.value)} placeholder="What's on your mind"/>
           <div className="w-full">
             <div className="justify-end flex"> 
               <button className="bg-yellow-400 hover:bg-yellow-500 text-white font-bold py-2 px-4 rounded" type="submit" onClick={sendPost}>Post</button>
