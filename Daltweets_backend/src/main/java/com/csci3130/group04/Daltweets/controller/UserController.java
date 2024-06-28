6de13367c2564764c644a4b6378894a94e08394e
Ibrahim Wassouf <ib981347@dal.ca>
2024-06-28 00:53:08 -0300

    fix: explicitly state necessary annotation imports

--- Daltweets_backend/src/main/java/com/csci3130/group04/Daltweets/controller/UserController.java
+++ Daltweets_backend/src/main/java/com/csci3130/group04/Daltweets/controller/UserController.java
@@ -4,7 +4,12 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.csci3130.group04.Daltweets.model.User;
 import com.csci3130.group04.Daltweets.model.Followers;
