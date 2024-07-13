fd9be768c1993f9481160dc030c2d849bc6e2b25
Hoang Le <syhoangloe@gmail.com>
2024-07-13 18:00:55 -0300

    feat: add group service

--- Daltweets_backend/src/main/java/com/csci3130/group04/Daltweets/service/Implementation/GroupServiceImpl.java
+++ Daltweets_backend/src/main/java/com/csci3130/group04/Daltweets/service/Implementation/GroupServiceImpl.java
@@ -1,18 +1,35 @@
 package com.csci3130.group04.Daltweets.service.Implementation;
 
 import com.csci3130.group04.Daltweets.model.Group;
+import com.csci3130.group04.Daltweets.repository.GroupRepository;
 import com.csci3130.group04.Daltweets.service.GroupService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GroupServiceImpl implements GroupService {
+
+    @Autowired
+    GroupRepository groupRepository;
     @Override
     public Group getGroupByName(Group group) {
-        return null;
+        if ( group == null ) {
+            throw new IllegalArgumentException("group is NULL to get");
+        }
+        String groupName = group.getName();
+        Group group_found = groupRepository.findGroupByName(groupName);
+        return group_found;
     }
 
     @Override
     public Group deleteGroup(Group group) {
-        return null;
+        if ( group == null ) {
+            throw new IllegalArgumentException("group is NULL to delete");
+        }
+
+        Group group_found = getGroupByName(group);
+        group_found.setIsDeleted(false);
+        groupRepository.save(group_found);
+        return group_found;
     }
 }
