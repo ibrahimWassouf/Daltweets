d14da99f063866f79526af5d046e50bd7e168a5d
Oluwatobiloba Onibudo <ol382056@dal.ca>
2024-07-13 22:24:33 -0300

    feat: adding  get group admin function to group service layer

--- Daltweets_backend/src/main/java/com/csci3130/group04/Daltweets/service/GroupService.java
+++ Daltweets_backend/src/main/java/com/csci3130/group04/Daltweets/service/GroupService.java
@@ -1,9 +1,13 @@
 package com.csci3130.group04.Daltweets.service;
 
+import java.util.List;
+
 import com.csci3130.group04.Daltweets.model.Group;
+import com.csci3130.group04.Daltweets.model.User;
 
 public interface GroupService {
     public Group getGroupByName( Group group );
     public boolean isValidToDelete(String username, String groupname);
     public Group deleteGroup ( Group group );
+    public List<User> getGroupAdmins(String groupName);
 }
