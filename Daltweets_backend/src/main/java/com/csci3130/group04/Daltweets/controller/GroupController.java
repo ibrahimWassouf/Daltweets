8cc2b2bc0ad9ecee29faa321a1596d420f245ca4
Oluwatobiloba Onibudo <ol382056@dal.ca>
2024-07-13 22:28:02 -0300

    feat: creating endpoint to get group admins

--- Daltweets_backend/src/main/java/com/csci3130/group04/Daltweets/controller/GroupController.java
+++ Daltweets_backend/src/main/java/com/csci3130/group04/Daltweets/controller/GroupController.java
@@ -1,12 +1,14 @@
 package com.csci3130.group04.Daltweets.controller;
 
 import com.csci3130.group04.Daltweets.model.Group;
+import com.csci3130.group04.Daltweets.model.User;
 import com.csci3130.group04.Daltweets.service.Implementation.GroupServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*", maxAge = 3600)
@@ -29,5 +31,14 @@ public class GroupController {
             return new ResponseEntity<>(group,HttpStatus.BAD_REQUEST);
         }
         return new ResponseEntity<>(group, HttpStatus.OK);
+    }
+
+    @GetMapping("/{groupName}/admins")
+    ResponseEntity<List<User>> getGroupAdmins(@PathVariable("groupName") String groupName)
+    {
+        if (groupName == null || groupName.isBlank()) return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        List<User> groupAdmins = groupService.getGroupAdmins(groupName);  
+        if (groupAdmins == null) return new ResponseEntity<>(null,HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(groupAdmins,HttpStatus.OK);
     }
 }
