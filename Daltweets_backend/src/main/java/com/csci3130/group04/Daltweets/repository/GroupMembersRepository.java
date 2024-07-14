7dadb0c5d1939f83e9e548680b786ba7d2a4f5c1
Oluwatobiloba Onibudo <ol382056@dal.ca>
2024-07-13 22:23:40 -0300

    feat: adding query to get group admins

--- Daltweets_backend/src/main/java/com/csci3130/group04/Daltweets/repository/GroupMembersRepository.java
+++ Daltweets_backend/src/main/java/com/csci3130/group04/Daltweets/repository/GroupMembersRepository.java
@@ -2,6 +2,8 @@ package com.csci3130.group04.Daltweets.repository;
 
 import com.csci3130.group04.Daltweets.model.Group;
 import com.csci3130.group04.Daltweets.model.GroupMembers;
+import com.csci3130.group04.Daltweets.model.User;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,4 +14,7 @@ import java.util.List;
 public interface GroupMembersRepository extends JpaRepository<GroupMembers,Integer> {
     @Query("SElECT gm FROM GroupMembers gm WHERE gm.user.username = :name")
     List<GroupMembers> findGroupMembersByUsername(@Param("name") String username);
+
+    @Query("SELECT gm.user FROM GroupMembers gm WHERE gm.group.name = :name  AND gm.isAdmin = true")
+    List<User> findAdminsByGroupName(@Param("name") String groupName);
 }
