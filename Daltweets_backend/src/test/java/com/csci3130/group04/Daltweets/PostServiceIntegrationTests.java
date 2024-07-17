43ed80887d07e1bc6dd8a1be11923776d61ecc94
Josh <53712177+JoshuaLomond@users.noreply.github.com>
2024-07-17 20:54:18 -0300

    feat: getAllPosts integration test

--- Daltweets_backend/src/test/java/com/csci3130/group04/Daltweets/PostServiceIntegrationTests.java
+++ Daltweets_backend/src/test/java/com/csci3130/group04/Daltweets/PostServiceIntegrationTests.java
@@ -1,12 +1,14 @@
 package com.csci3130.group04.Daltweets;
 
+import com.csci3130.group04.Daltweets.model.Followers;
 import com.csci3130.group04.Daltweets.model.Post;
 import com.csci3130.group04.Daltweets.model.User;
 import com.csci3130.group04.Daltweets.model.User.Role;
 
 import com.csci3130.group04.Daltweets.repository.PostRepository;
 import com.csci3130.group04.Daltweets.repository.UserRepository;
-
+import com.csci3130.group04.Daltweets.repository.FollowersRepository;
+import com.csci3130.group04.Daltweets.service.Implementation.FollowersServiceImpl;
 import com.csci3130.group04.Daltweets.service.Implementation.PostServiceImpl;
 
 import org.junit.jupiter.api.AfterEach;
@@ -44,19 +46,27 @@ class PostServiceIntegrationTests {
     private PostServiceImpl postService;
 
     @Autowired
-    private UserRepository userRepository;
+    private PostRepository postRepository;
 
     @Autowired
-    private PostRepository postRepository;
+    private FollowersServiceImpl followersService;
+
+    @Autowired
+    private FollowersRepository followersRepository;
+
+    @Autowired
+    private UserRepository userRepository;
 
     @BeforeEach
     void setup() {
+        followersRepository.deleteAll();
         postRepository.deleteAll();
         userRepository.deleteAll();
     }
 
     @AfterEach
     void teardown() {
+        followersRepository.deleteAll();
         postRepository.deleteAll();
         userRepository.deleteAll();
     }
@@ -108,6 +118,58 @@ class PostServiceIntegrationTests {
 
         assertNull(response.getBody());
         assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    }
+
+    @Test
+    void getAllPostsIntegrationTest() {
+        // Create users
+        User user = new User(1, "I follow people", "Following", "following@dal.ca", LocalDateTime.now(), false,
+                Role.SUPERADMIN, User.Status.ONLINE);
+        User user2 = new User(1, "I will be followed", "Followed1", "followed1@dal.ca", LocalDateTime.now(), false,
+                Role.SUPERADMIN, User.Status.ONLINE);
+        User user3 = new User(1, "I'll also be followed", "Followed2", "followed2@dal.ca", LocalDateTime.now(), false,
+                Role.SUPERADMIN, User.Status.ONLINE);
+
+        user = userRepository.save(user);
+        user2 = userRepository.save(user2);
+        user3 = userRepository.save(user3);
+
+        // Add followers
+        Followers firstFollow = followersService.addFollower(user, user2);
+        Followers secondFollow = followersService.addFollower(user, user3);
+
+        followersRepository.save(firstFollow);
+        followersRepository.save(secondFollow);
+
+        // Create posts
+        Post post2 = new Post();
+        post2.setUser(user2);
+        post2.setText("User 2's Post");
+        post2.setDateCreated(LocalDateTime.now());
+
+        Post post3 = new Post();
+        post3.setUser(user3);
+        post3.setText("User 3's Post");
+        post3.setDateCreated(LocalDateTime.now());
+
+        postRepository.save(post2);
+        postRepository.save(post3);
+
+        // Make the GET request to retrieve all posts for the specified user
+        ResponseEntity<Post[]> response = restTemplate.getForEntity(
+                "http://localhost:" + port + "/api/post/Following/all", Post[].class);
+
+        // Verify the response
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertNotNull(response.getBody());
+        assertEquals(2, response.getBody().length);
+        List<Post> posts = List.of(response.getBody());
+
+        // Verify the content of the posts
+        assertEquals("User 2's Post", posts.get(0).getText());
+        assertEquals("Followed1", posts.get(0).getUser().getUsername());
+        assertEquals("User 3's Post", posts.get(1).getText());
+        assertEquals("Followed2", posts.get(1).getUser().getUsername());
     }
 
 }
