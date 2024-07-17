d0c6ab551c90dab63af1a529b522804742ddb0ef
Ibrahim Wassouf <ib981347@dal.ca>
2024-07-17 18:25:35 -0300

    feat: add more styling to tabs in admin page

--- Daltweets/src/pages/AdminPage.jsx
+++ Daltweets/src/pages/AdminPage.jsx
@@ -3,13 +3,33 @@ import SignupRequests from "../components/SignupRequests";
 import UserList from "../components/UserList";
 
 export default function AdminPage() {
+  const tabs = [
+    {
+      title: "Signup Requests",
+    },
+    {
+      title: "Users",
+    },
+    {
+      title: "Groups",
+    },
+  ];
   return (
     <>
       <TabGroup className="mx-auto w-full h-min">
-        <TabList className="box-border m-auto w-10/12 space-x-4 flex justify-center">
-          <Tab>Signup Requests</Tab>
-          <Tab>Users</Tab>
-          <Tab>Groups</Tab>
+        <TabList className="box-border m-auto my-5 w-full space-x-4 flex justify-center">
+          <div className="w-1/3 flex justify-between mr-12 pr-12">
+            {tabs.map((t, idx) => {
+              return (
+                <Tab
+                  key={idx}
+                  className="rounded-full py-1 px-3 text-sm/6 font-semibold focus:outline-none data-[selected]:bg-gold/50 data-[hover]:bg-gold/50 data-[selected]:data-[hover]:bg-gold/50 data-[focus]:outline-1 data-[focus]:outline-white"
+                >
+                  {t.title}
+                </Tab>
+              );
+            })}
+          </div>
         </TabList>
         <TabPanels className="box-border w-full h-max">
           <TabPanel className="box-border w-full">
