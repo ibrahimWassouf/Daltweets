0e2821656a44d4b79771a6f8d82c67273951cd04
Josh <53712177+JoshuaLomond@users.noreply.github.com>
2024-07-18 14:20:24 -0300

    feat(UpdateUser): Improve styling for better consistency with overall website theme

--- Daltweets/src/pages/UpdateUser.jsx
+++ Daltweets/src/pages/UpdateUser.jsx
@@ -11,8 +11,9 @@ const UpdateUser = () => {
   const [accountDeleted, setAccountDeleted] = useState(user.accountDeleted);
   const [role, setRole] = useState(user.role);
   const [status, setStatus] = useState(user.status);
+  const [error, setError] = useState("");
 
-  const updateuser = (e) => {
+  const updateUser = (e) => {
     e.preventDefault();
     const formData = {
       id,
@@ -33,63 +34,113 @@ const UpdateUser = () => {
         console.log(response);
       })
       .catch((error) => {
-        alert("Update fail");
+        setError("Update failed: " + error.response.data.message);
         console.log(error);
       });
   };
 
   return (
-    <div className="w-screen h-screen ">
-      <div className="justify-center flex h-12 ">Update User</div>
-      <div className="justify-center flex ">
-        <form onSubmit={updateuser} className="flex flex-col gap-4 w-72">
-          <input
-            type="text"
-            name="username"
-            defaultValue={user.username}
-            placeholder="Name"
-            onChange={(e) => setUserName(e.target.value)}
-            className="p-3 rounded-md border-slate-400 border-2"
-          />
-          <input
-            type="text"
-            name="email"
-            defaultValue={user.email}
-            placeholder="Email"
-            onChange={(e) => setEmail(e.target.value)}
-            className="p-3 rounded-md border-slate-400 border-2"
-          />
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+        Update User
+      </h2>
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-md">
+        <form onSubmit={updateUser} className="space-y-6">
+          <div>
+            <label
+              htmlFor="username"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Username
+            </label>
+            <div>
+              <input
+                id="username"
+                type="text"
+                value={username}
+                onChange={(e) => setUserName(e.target.value)}
+                className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Email
+            </label>
+            <div>
+              <input
+                id="email"
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
           {user.role === "SUPERADMIN" && (
-            <input
-              type="text"
-              name="role"
-              defaultValue={user.role}
-              placeholder="Role"
-              onChange={(e) => setRole(e.target.value.toUpperCase())}
-              className="p-3 rounded-md border-slate-400 border-2"
-            />
+            <div>
+              <label
+                htmlFor="role"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Role
+              </label>
+              <div>
+                <input
+                  id="role"
+                  type="text"
+                  value={role}
+                  onChange={(e) => setRole(e.target.value.toUpperCase())}
+                  className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
           )}
-          <input
-            type="text"
-            name="status"
-            defaultValue={user.status}
-            placeholder="Status"
-            onChange={(e) => setStatus(e.target.value.toUpperCase())}
-            className="p-3 rounded-md border-slate-400 border-2"
-          />
-          <textarea
-            name="bio"
-            defaultValue={user.bio}
-            placeholder="Bio"
-            onChange={(e) => setBio(e.target.value)}
-            className="p-3 rounded-md border-slate-400 border-2 h-36 "
-          />
-          <button
-            type="submit"
-            className="px-4 py-2 rounded bg-black text-gold font-bold cursor-pointer border-2 border-black"
-          >
-            Submit
-          </button>
+          <div>
+            <label
+              htmlFor="status"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Status
+            </label>
+            <div>
+              <input
+                id="status"
+                type="text"
+                value={status}
+                onChange={(e) => setStatus(e.target.value.toUpperCase())}
+                className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div>
+            <label
+              htmlFor="bio"
+              className="block text-sm font-medium leading-6 text-gray-900"
+            >
+              Bio
+            </label>
+            <div>
+              <textarea
+                id="bio"
+                value={bio}
+                onChange={(e) => setBio(e.target.value)}
+                className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 h-36"
+              />
+            </div>
+          </div>
+          {error && <div className="text-red-500 mt-4">{error}</div>}
+          <div>
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-black px-3 py-1.5 text-sm font-semibold leading-6 text-yellow-500 shadow-sm hover:bg-yellow-500 hover:text-black focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Submit
+            </button>
+          </div>
         </form>
       </div>
     </div>
