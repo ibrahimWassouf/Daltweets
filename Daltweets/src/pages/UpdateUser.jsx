376f3c738eb9121d38ff515211f74ba92aed4c77
Josh <53712177+JoshuaLomond@users.noreply.github.com>
2024-07-18 20:04:35 -0300

    fix(UpdateUser): email field is readOnly and visually distinct

--- Daltweets/src/pages/UpdateUser.jsx
+++ Daltweets/src/pages/UpdateUser.jsx
@@ -75,8 +75,8 @@ const UpdateUser = () => {
                 id="email"
                 type="email"
                 value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                readOnly
+                className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 bg-gray-200 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
           </div>
