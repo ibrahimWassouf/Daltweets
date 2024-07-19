c968915a5756e2bf5071333a2682e07f71d43999
Hoang Le <syhoangloe@gmail.com>
2024-07-19 12:49:56 -0300

    feat: add group to navbar

--- Daltweets/src/components/NavBar.jsx
+++ Daltweets/src/components/NavBar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { CiLogout } from 'react-icons/ci'
 import { FaHome, FaSearch, FaUserCircle, FaUserFriends } from 'react-icons/fa'
 import { IoCreateOutline } from 'react-icons/io5'
+import { MdGroups } from 'react-icons/md'
 import { Link } from 'react-router-dom'
 
 
@@ -28,6 +29,12 @@ const NavBar = () => {
                 <Link to="/create"> {/** When add the link change from a to Link component*/}
                     <IoCreateOutline className="inline-block w-6 h-6 mr-2 -mt-2"/> 
                     Create
+                </Link>
+            </li>
+            <li className="mb-20 rounded hover:shadow hover:bg-blue-500 py-2"> 
+                <Link to="/group"> {/** When add the link change from a to Link component*/}
+                    <MdGroups className="inline-block w-6 h-6 mr-2 -mt-2"/> 
+                    Group
                 </Link>
             </li>
             <li className="mb-20 rounded hover:shadow hover:bg-blue-500 py-2"> 
