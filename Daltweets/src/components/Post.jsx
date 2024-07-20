6c655e2e85eb3b908038e77950a50178a3b02810
Oluwatobiloba Onibudo <ol382056@dal.ca>
2024-07-20 20:20:56 -0300

    feat: added user profile link to post component

--- Daltweets/src/components/Post.jsx
+++ Daltweets/src/components/Post.jsx
@@ -1,19 +1,69 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactTimeAgo from "react-time-ago";
 import TimeAgo from "javascript-time-ago";
 
 import en from "javascript-time-ago/locale/en";
 import { FaRegComment, FaRegHeart } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 TimeAgo.addDefaultLocale(en);
 
 function Post({ text, username, dateCreated, ...props }) {
+  const [followers,setFollowers] = useState([]);
+  const [followings,setFollowings] = useState([]);
+  let user = JSON.parse(localStorage.getItem("user"))
+  const fetchFollowing = async () => {
+    await axios
+      .get(`${import.meta.env.VITE_BACKEND_BASE_URL}/api/followers/${user.username}/following`)
+      .then((response) => {
+        setFollowings(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const fetchFollowers = async () => {
+    await axios
+      .get(`${import.meta.env.VITE_BACKEND_BASE_URL}/api/followers/${user.username}/followers`)
+      .then((response) => {
+        setFollowers(response.data);
+        console.log(response.data)
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const profileDetails = (friendName) => {
+    const isFollowing =
+      followings.find((user) => user.username === friendName) !== undefined;
+    const isFollower =
+      followers.find((user) => user.username === friendName) !== undefined; 
+    console.log(followers)
+    return {username: friendName, isFriend: (isFollower || isFollowing) }
+  };
+
+  useEffect(
+    ()=>{
+      fetchFollowers();
+      fetchFollowing();
+    },[]
+  )
+
   return (
     <div>
       <div className="border mb-1 border-black w-full">
         <div className="pl-2">
           <div>
+          <Link
+                          to={`/profile`}
+                          state={profileDetails(username)}
+                          className="text-black-500 hover:text-yellow-700"
+                          >
             <span className="font-bold">{username}</span>
+            </Link>
             <span className="pl-1 text-gray-500">
               {" "}
               <ReactTimeAgo date={Date.parse(dateCreated)}></ReactTimeAgo>{" "}
