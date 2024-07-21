d413c399720e37a8998ed13f064ad678986c3f32
Espen Mejia-Ortega <espenmmo@gmail.com>
2024-07-21 08:32:20 -0300

    feat: styled profile page

--- Daltweets/src/pages/Profile.jsx
+++ Daltweets/src/pages/Profile.jsx
@@ -57,32 +57,44 @@ function Profile() {
     return (
         <div className="container rounded bg-gray-100 mt-3 mb-3 mr-3 ml-3">
             <div className="grid">
-                <div className="grid-cols-1 ml-5 mt-10">
-                    <div className="font-bold text-3xl">{user.username}
-                    {isLoggedInUser  ?   
-                        <button className="bg-blue-300 hover:bg-yellow-200 rounded-full ml-1 py-0 px-2 text-base" onClick={routeChange}>
-                            <FaGear className="mr-1 inline-block mb-1"/>Edit
-                        </button> : (<></>)}
+                <div className="my-2 mx-2 border-gray-300 border-2 bg-white rounded-lg">
+                    <div>
+                        <img src="cs-banner.jpg" alt="" className=" w-full h-auto"/>
                     </div>
-                    <div className="text-base">{user.email}</div>
-                    { 
-                      isFriend || isLoggedInUser  ?   
-                        ( <div className="text-base italic flex">{user.status}<GoDotFill color={statusColor} className="flex items-center justify-center"/></div>) 
-                     : 
-                     (<></>)
-                    }
-                </div>
-                <div className="grid-cols-1 border-right">
-                    <div className="p-3 py-5">
-                        <div className="row mt-3">
-                            <div className="text-xl">
-                                <h2>About</h2>
-                            </div>
-                            <div className="col-md-12">
-                                <p>{user.bio}</p>
+                    <div className="grid-cols-1 ml-5 mt-5">
+                        <img src="favicon.ico" alt="" className=" w-30  max-h-40 border-gray-200 border-4 rounded-full"/>
+                        <div className="font-bold text-3xl">{user.username}
+                        {isLoggedInUser  ?   
+                            <button className="bg-blue-300 hover:bg-yellow-200 rounded-full ml-1 py-0 px-2 text-base" onClick={routeChange}>
+                                <FaGear className="mr-1 inline-block mb-1"/>Edit
+                            </button> : (<></>)}
+                        </div>
+                        <div className="text-base">{user.email}</div>
+                        { 
+                        isFriend || isLoggedInUser  ?   
+                            ( <div className="text-base italic flex">{user.status}<GoDotFill color={statusColor} className="flex items-center justify-center"/></div>) 
+                        : 
+                        (<></>)
+                        }
+                    </div>
+                    <div className="grid-cols-1 border-right">
+                        <div className="p-3 py-5">
+                            <div className="row mx-2">
+                                <div className="text-2xl">
+                                    <h2 className="font-bold">About</h2>
+                                </div>
+                                <div className="col-md-12">
+                                    <p>{user.bio}</p>
+                                </div>
                             </div>
                         </div>
                     </div>
+                </div>
+                <div className="grid-cols-1 mx-2 my-2 border-gray-300 border-2 bg-white rounded-lg">
+                    <h2 className="text-xl font-bold ml-3 my-3">Groups</h2>
+                </div>
+                <div className="grid-cols-1 mx-2 my-2 border-gray-300 border-2 bg-white rounded-lg">
+                    <h2 className="text-xl font-bold ml-3 my-3">Posts</h2>
                 </div>
             </div>
         </div>
