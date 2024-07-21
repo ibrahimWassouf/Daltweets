b35806043e106e6080f1fccaeb3294fe80a4b05d
Oluwatobiloba Onibudo <ol382056@dal.ca>
2024-07-21 16:56:19 -0300

    feat: created post comment service implementation

---
+++ Daltweets_backend/src/main/java/com/csci3130/group04/Daltweets/service/Implementation/PostCommentServiceImpl.java
@@ -0,0 +1,35 @@
+package com.csci3130.group04.Daltweets.service.Implementation;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.csci3130.group04.Daltweets.model.Post;
+import com.csci3130.group04.Daltweets.model.PostComment;
+import com.csci3130.group04.Daltweets.model.User;
+import com.csci3130.group04.Daltweets.repository.PostCommentRepository;
+import com.csci3130.group04.Daltweets.service.PostCommentService;
+
+@Service
+public class PostCommentServiceImpl implements PostCommentService {
+
+    @Autowired
+    PostCommentRepository postCommentRepository;
+
+    @Override
+    public PostComment createPostComment(Post post, User user, String comment) {
+        if (post == null || user == null || comment == null) return null;
+        LocalDateTime dateCreated = LocalDateTime.now();
+        PostComment postComment = new PostComment(0,user,post, comment, dateCreated);
+        return postCommentRepository.save(postComment);
+    }
+
+    @Override
+    public List<PostComment> getPostComments(Post post) {
+       if (post == null) return null;
+       return postCommentRepository.findByPostId(post.getPostID());
+    }
+    
+}
---
+++ Daltweets_backend/src/main/java/com/csci3130/group04/Daltweets/service/PostCommentService.java
@@ -0,0 +1,12 @@
+package com.csci3130.group04.Daltweets.service;
+
+import java.util.List;
+
+import com.csci3130.group04.Daltweets.model.Post;
+import com.csci3130.group04.Daltweets.model.PostComment;
+import com.csci3130.group04.Daltweets.model.User;
+
+public interface PostCommentService {
+    PostComment createPostComment(Post post, User user, String comment);
+    List<PostComment> getPostComments(Post post);
+} 
