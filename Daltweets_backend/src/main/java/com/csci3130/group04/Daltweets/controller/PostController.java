db36cf2feb3e517c967bda2b9b25385b7da31a59
Oluwatobiloba Onibudo <ol382056@dal.ca>
2024-07-21 16:56:29 -0300

    feat: adding endpoints to post comment controller

--- Daltweets_backend/src/main/java/com/csci3130/group04/Daltweets/controller/PostController.java
+++ Daltweets_backend/src/main/java/com/csci3130/group04/Daltweets/controller/PostController.java
@@ -1,18 +1,23 @@
 package com.csci3130.group04.Daltweets.controller;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
 
 import com.csci3130.group04.Daltweets.service.Implementation.GroupServiceImpl;
 import com.csci3130.group04.Daltweets.service.Implementation.UserServiceImplementation;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.csci3130.group04.Daltweets.model.Post;
+import com.csci3130.group04.Daltweets.model.PostComment;
 import com.csci3130.group04.Daltweets.model.User;
 import com.csci3130.group04.Daltweets.service.FollowersService;
+import com.csci3130.group04.Daltweets.service.PostCommentService;
 import com.csci3130.group04.Daltweets.service.PostService;
 import com.csci3130.group04.Daltweets.service.UserService;
 
@@ -22,6 +27,9 @@ import com.csci3130.group04.Daltweets.service.UserService;
 public class PostController {
     @Autowired
     PostService postService;
+
+    @Autowired 
+    PostCommentService postCommentService;
 
     @Autowired
     UserServiceImplementation userService;
@@ -57,6 +65,47 @@ public class PostController {
         }
         List<Post> groupposts = postService.getPostsByUsers(groupmembers);
         return new ResponseEntity<>(groupposts,HttpStatus.OK);
+    }
+
+    @PostMapping("/comment/create")
+    ResponseEntity<PostComment> createPostComment(@RequestBody Map<String, String> requestBody)
+    {
+        int postId = Integer.parseInt(requestBody.get("postId"));
+        String username = requestBody.get("username");
+        String comment = requestBody.get("comment");
+        if (postId < 0 || username == null || comment == null ) 
+        {
+            return ResponseEntity.badRequest().body(null);
+        }
+
+        Post post = postService.getPostById(postId);
+        User user = userService.getUserByName(username);
+
+        if (post == null || user == null)
+        {
+            return ResponseEntity.notFound().build();
+        }
+
+        PostComment createdPostComment = postCommentService.createPostComment(post,user,comment);
+        return ResponseEntity.ok().body(createdPostComment);
+    } 
+
+    @GetMapping("/comment/get/{postId}")
+    ResponseEntity<List<PostComment>> getPostComments(@PathVariable("postId") int postId ){
+        if (postId < 0)
+        {
+            return ResponseEntity.badRequest().body(null);
+        }
+
+        Post post = postService.getPostById(postId);
+
+        List<PostComment> postComments = postCommentService.getPostComments(post);
+
+        if (postComments == null)
+        {
+            return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED).body(null);
+        }
+        return ResponseEntity.ok().body(postComments);
     }
 
 }
